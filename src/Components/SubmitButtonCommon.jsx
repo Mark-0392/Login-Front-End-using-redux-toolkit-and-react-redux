7d883e827a522e6a0b4bcb81f9277b6f33e3b3bf
MARK X DANIEL <markxdaniel@MARKs-MacBook-Air.local>
2024-12-21 09:12:54 +0530

    completed the edit tasks

--- src/Components/SubmitButtonCommon.jsx
+++ src/Components/SubmitButtonCommon.jsx
@@ -1,6 +1,6 @@
 import { useNavigation } from 'react-router-dom'
 
-const SubmitButtonCommon = ({ text, height, navigationState }) => {
+const SubmitButtonCommon = ({ text, width, height, navigationState }) => {
   const navigation = useNavigation()
   const isSubmitting = navigation.state === 'submitting'
 
@@ -8,7 +8,7 @@ const SubmitButtonCommon = ({ text, height, navigationState }) => {
     <button
       type="submit"
       disabled={isSubmitting}
-      className={`w-full ${height} flex justify-center items-center py-2 px-4 border border-transparent rounded shadow-md text-sm lg:text-xl font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 `}
+      className={`${width} ${height} flex justify-center items-center py-2 px-4 border border-transparent rounded shadow-md text-sm lg:text-xl font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 `}
     >
       {isSubmitting ? navigationState : text || 'Submit'}
     </button>
--- src/Pages/EditTasks.jsx
+++ src/Pages/EditTasks.jsx
@@ -1,33 +1,55 @@
 import { Form } from 'react-router-dom'
 import CommonFormLayout from '../Components/CommonFormLayout'
+import SubmitButtonCommon from '../Components/SubmitButtonCommon'
 
 const EditTasks = () => {
   return (
     <div className="min-h-screen grid place-items-center bg-slate-800 lg:bg-white">
-      <Form method="POST" className="w-11/12 max-w-xl grid gap-y-4">
+      <Form className="w-11/12 max-w-xl grid gap-y-4">
         <h1 className="text-lg font-semibold text-white lg:text-black lg:text-xl lg:text-center">
           Edit Task
         </h1>
-
-        <CommonFormLayout
+        <div className="flex gap-1 justify-center items-center">
+          <label
+            htmlFor="task"
+            className="block basis-1/3 text-sm md:text-lg font-medium  text-white mb-1 px-2 lg:text-black"
+          >
+            Task
+          </label>
+          <input
+            type="text"
+            id="task"
+            placeholder="Enter your task here"
+            name="task"
+            className="w-full h-10 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 text-base text-gray-500 p-2"
+          />
+        </div>
+        {/* <CommonFormLayout
           type="text"
           name="name"
           defaultValue=""
           placeholder=""
           label="Task"
-        />
+        /> */}
         <div className="flex items-center gap-x-4 ">
           <label
             htmlFor="isCompleted"
-            className="block   text-sm font-medium lg:text-lg text-white mb-1 px-2 lg:text-black"
+            className="block basis-1/4 text-sm md:text-lg font-medium  text-white mb-1 px-2 lg:text-black"
           >
-            Completed :
+            Task Completed
           </label>
           <input
             type="checkbox"
             id="isCompleted"
             name="completed"
-            className="h-3 w-3 lg:h-5   lg:w-5 rounded-md border-gray-500"
+            className=" h-4 w-4 rounded-md border-gray-500 -ml-2"
+          />
+        </div>
+        <div className="">
+          <SubmitButtonCommon
+            text="Confirm my Edit"
+            navigationState="saving..."
+            width="w-full"
           />
         </div>
       </Form>
